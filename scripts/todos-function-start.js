5c04ea9d0e06b9adc00840bba49d3861d2c6b9c7
Nathan Waterman <nathanjwuk@gmail.com>
2018-08-16 07:21:51 -0500

    Added a checkbox and remove button

--- scripts/todos-function-start.js
+++ scripts/todos-function-start.js
@@ -1,45 +1,64 @@
 const loadTodos = function () {
-  // Call pre-filtered list of Todos
-  if (localStorage.getItem('todos') != null) {
-    return JSON.parse(localStorage.getItem('todos'))
-  } else {
-    return []
-  }
+    // Call pre-filtered list of Todos
+    if (localStorage.getItem('todos') != null) {
+        return JSON.parse(localStorage.getItem('todos'))
+    } else {
+        return []
+    }
 }
 
 const saveTodo = function (title, body) {
-  let todos = loadTodos()
-  todos.push({title: title,
-    body: body,
-    complete: false})
-  localStorage.setItem('todos', JSON.stringify(todos))
+    let todos = loadTodos()
+    todos.push({title: title,
+        body: body,
+        complete: false})
+    localStorage.setItem('todos', JSON.stringify(todos))
 }
 
 const displayTodos = function (todos, filter) {
-  const filteredTodos = todos.filter(function (todo) {
-    if (todo.title.toLowerCase().includes(filters.searchText.toLowerCase()) ||
-        todo.body.toLowerCase().includes(filters.searchText.toLowerCase())) {
-      debugger
-          return todo.title.toLowerCase().includes(filters.searchText.toLowerCase())
-    }
-  })
-  document.querySelector('#todos').innerHTML = ''
-  filteredTodos.forEach(function (todo) {
-    generateTodoDOM(todo)
-  })
-  summaryDOM(todos, filteredTodos)
+    const filteredTodos = todos.filter(function (todo) {
+        if (todo.title.toLowerCase().includes(filters.searchText.toLowerCase()) ||
+            todo.body.toLowerCase().includes(filters.searchText.toLowerCase())) {
+        // debugger
+            return todo.title.toLowerCase().includes(filters.searchText.toLowerCase())
+        }
+    })
+    document.querySelector('#todos').innerHTML = ''
+    filteredTodos.forEach(function (todo) {
+        generateTodoDOM(todo)
+    })
+    summaryDOM(todos, filteredTodos)
 }
 
 const generateTodoDOM = function (todo) {
-  const todoTitle1 = document.createElement('h2')
-  const todoBody1 = document.createElement('p')
-  todoTitle1.textContent = todo.title
-  todoBody1.textContent = todo.body
-  document.querySelector('#todos').appendChild(todoTitle1)
-  document.querySelector('#todos').appendChild(todoBody1)
+    const todoRoot = document.createElement('div')
+    const todoCheck = document.createElement('input')
+    const todoText = document.createElement('span')
+    const todoDelete = document.createElement('button')
+    const todoTitle = document.createElement('h2')
+    
+    // setup checkbox
+    todoCheck.setAttribute('type', 'checkbox')
+    todoRoot.appendChild(todoCheck)
+
+    // setup span element
+    todoText.textContent = todo.body
+    todoRoot.appendChild(todoText)
+
+    // setup delete button
+    todoDelete.textContent = 'Remove'
+    todoRoot.appendChild(todoDelete)
+
+    todoTitle.textContent = todo.title
+
+    document.querySelector('#todos').appendChild(todoTitle)
+    document.querySelector('#todos').appendChild(todoRoot)
+    
+
+    return todoRoot
 }
 
 const summaryDOM = function (todos, filteredTodos) {
-  summary.textContent = `You have ${(todos.length)} total Todos 
-  and ${filteredTodos.length} Todos left that match your search criteria`
+    summary.textContent = `You have ${(todos.length)} total Todos 
+    and ${filteredTodos.length} Todos left that match your search criteria`
 }
